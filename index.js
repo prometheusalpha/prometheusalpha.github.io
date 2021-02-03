df51ec529b76d68288564bcc45f585815fad44eb
PrometheusAlpha <75623745+PrometheusAlpha@users.noreply.github.com>
2021-02-03 11:01:28 +0700

    .

---
+++ index.js
@@ -0,0 +1,68 @@
+function copy_effect() {
+  if (document.getElementById("copy-alert").className != "show") {
+    document.getElementById("copy-alert").classList.add("show");
+  }
+  setTimeout(function () {
+    document.getElementById("copy-alert").classList.remove("show");
+  }, 1000);
+}
+
+function copyText() {
+  navigator.clipboard.writeText("hoangnguyen.hn212.hn@gmail.com");
+  copy_effect();
+}
+
+class TxtType {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  }
+  tick() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 150;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  }
+}
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+};
