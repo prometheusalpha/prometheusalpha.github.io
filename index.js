728f0e9867ea37946a315dc62105a3bdf1cfe4de
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-08-17 21:37:48 +0700

    CHANGE sass to scss, JS refactor

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-function copy_effect() {
-  if (document.getElementById("copy-alert").className != "show") {
-    document.getElementById("copy-alert").classList.add("show");
-  }
-  setTimeout(function () {
-    document.getElementById("copy-alert").classList.remove("show");
-  }, 1000);
-}
-
-function copyText() {
+document.querySelector("#email").addEventListener("click", () => {
   navigator.clipboard.writeText("hoangnguyen.hn212.hn@gmail.com");
-  copy_effect();
-}
+})
 
 class TxtType {
-  constructor(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
+  constructor(target, alternating_words) {
+    this.alternating_words = alternating_words;
+    this.target = target;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 150;
     this.txt = "";
-    this.tick();
     this.isDeleting = false;
+    this.tick();
   }
   tick() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+    var i = this.loopNum % this.alternating_words.length;
+    var fullTxt = this.alternating_words[i];
 
     if (this.isDeleting) {
       this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -32,22 +21,22 @@ class TxtType {
       this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+    this.target.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
     var that = this;
-    var delta = 150;
+    let delta = 150;
 
     if (this.isDeleting) {
       delta /= 2;
     }
 
     if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
+      delta = 1000;
       this.isDeleting = true;
     } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       this.loopNum++;
-      delta = 1000;
+      delta = 500;
     }
 
     setTimeout(function () {
@@ -57,12 +46,9 @@ class TxtType {
 }
 
 window.onload = function () {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
+  var element = document.querySelector("#typewrite");
+  var alternating_words = element.getAttribute("data-type");
+  if (alternating_words) {
+    new TxtType(element, JSON.parse(alternating_words));
   }
 };
