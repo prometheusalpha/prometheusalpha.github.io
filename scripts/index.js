724d8d15e5843c01448fc403fa3b782ef70a5aa3
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2023-01-15 15:17:13 +0700

    Update profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-var svgCursor = document.getElementById('svg-cursor');
+// var svgCursor = document.getElementById('svg-cursor');
 
-document.querySelector("#select").addEventListener("mousemove", function (event) {
-  svgCursor.style.display = 'block';
-  moveCursor(event);
-});
+// document.querySelector("#select").addEventListener("mousemove", function (event) {
+//   svgCursor.style.display = 'block';
+//   moveCursor(event);
+// });
 
-document.querySelector("#select").addEventListener("mouseleave", function () {
-  svgCursor.style.display = 'none';
-});
+// document.querySelector("#select").addEventListener("mouseleave", function () {
+//   svgCursor.style.display = 'none';
+// });
 
-function moveCursor(e) {
-  var x = e.clientX + window.pageXOffset + 5,
-    y = e.clientY + window.pageYOffset;
-  svgCursor.setAttribute('style', 'left: ' + x + 'px; top: ' + y + 'px');
-}
+// function moveCursor(e) {
+//   var x = e.clientX + window.pageXOffset + 5,
+//     y = e.clientY + window.pageYOffset - 500;
+//   svgCursor.setAttribute('style', 'left: ' + x + 'px; top: ' + y + 'px');
+// }
