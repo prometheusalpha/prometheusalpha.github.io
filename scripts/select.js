b0a330aa18e865db148c997e0f3cb3c09a0d57db
hoangnh280 <hoangnh280@ghtk.co>
2023-01-19 10:49:53 +0700

    feat(glitch): add glitch - color to svg

--- scripts/select.js
+++ scripts/select.js
@@ -20,6 +20,11 @@ document.querySelector("#cd").addEventListener("keyup", function (event) {
   }
 });
 
+// scroll to top on click
+document.querySelector("#top").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 function suggest() {
   let cdVal = document.querySelector("#cd").value;
   // for each
@@ -43,11 +48,58 @@ function tabListener() {
 
 function enterListener() {
   let placeholder = document.querySelector("#placeholder").innerText;
-  // check if placeholder is empty or not in values
   if (placeholder == "" || !values.includes(placeholder)) {
     return;
   }
   window.location.href = `#${placeholder}`;
-  // clear input
+  scrollToSection(placeholder);
   document.querySelector("#cd").value = "";
 }
+
+// scroll to selected section function
+function scrollToSection(hash) {
+  if (hash == "") {
+    return;
+  }
+  let section = document.querySelector("#" + hash);
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substring(0, index) +
+    replacement +
+    this.substring(index + replacement.length)
+  );
+};
+
+async function convertString(str1, str2) {
+  let len = strings[0].length;
+  let swappeds = new Array(len).fill(false);
+  while (swappeds.includes(false)) {
+    // random position
+    let ranPos = Math.floor(Math.random() * len);
+    if (swappeds[ranPos]) {
+      continue;
+    }
+    // replace a character
+    str1 = str1.replaceAt(ranPos, str2[ranPos]);
+    document.querySelector("#glitch").innerText = str1;
+    await sleep(60);
+    swappeds[ranPos] = true;
+  }
+}
+
+let strings = ["Nguyễn Huy Hoàng", "a developer     "];
+
+async function animate() {
+  convertString(strings[0], strings[1]);
+  await sleep(3000);
+  convertString(strings[1], strings[0]);
+}
+
+setInterval(animate, 5000);
